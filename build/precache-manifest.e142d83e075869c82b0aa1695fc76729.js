d6500f09164452925e58b882bfdde5fe30a0106b
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-05-30 20:39:38 +0300

    deploy github pages

---
+++ build/precache-manifest.e142d83e075869c82b0aa1695fc76729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eef52f51bdc69a7fc4d9beb79608c7",
+    "url": "/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "9f6f662f9e664204e35a",
+    "url": "/React-To-Do-List/static/css/main.47761bfd.chunk.css"
+  },
+  {
+    "revision": "2e2e758656697ff62fb7",
+    "url": "/React-To-Do-List/static/js/2.c53ecabc.chunk.js"
+  },
+  {
+    "revision": "9f6f662f9e664204e35a",
+    "url": "/React-To-Do-List/static/js/main.0a431980.chunk.js"
+  },
+  {
+    "revision": "479da4bc49464f71ee85",
+    "url": "/React-To-Do-List/static/js/runtime-main.d37aedff.js"
+  }
+]);
